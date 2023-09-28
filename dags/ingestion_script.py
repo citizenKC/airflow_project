2ec0e0539d6ce028eca961ad3a0ff2755a011f92
Roman G <geromit@yandex.ru>
2023-09-28 20:53:46 +0300

    Updated README.md file and ingestion_script.py

--- dags/ingestion_script.py
+++ dags/ingestion_script.py
@@ -3,14 +3,18 @@ from sqlalchemy import create_engine
 from time import time
 import os
 from dotenv import load_dotenv
- 
 load_dotenv()
+
+USER = os.environ.get('PG_USER')
+PASS = os.environ.get('PG_PASS')
+DB = os.environ.get('PG_DB')
+PRT = os.environ.get("PG_PORT_2")
 
 def ingest_data(parquet_file, table_name):
     parquet_file = pq.ParquetFile(parquet_file)
     # trips = parquet_file.read().to_pandas()
 
-    engine = create_engine(f"postgresql://{os.environ.get('PG_USER')}:{os.environ.get('PG_PASS')}@de_postgres:5432/{os.environ.get('PG_DB')}")
+    engine = create_engine(f"postgresql://{USER}:{PASS}@de_postgres:{PRT}/{DB}")
 
     # trips.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace", index=False)
 
